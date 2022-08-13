b2c7f51dedbb6ab32b559905b958f352ca514612
narutiga <100295602+narutiga@users.noreply.github.com>
2022-08-13 12:53:57 +0900

    Next.js +ESlint +Prettier +TailwindCSS +Mantine

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Index() {
+  return (
+    <div>
+      <Head>
+        <title>next</title>
+      </Head>
+      <h1>test</h1>
+    </div>
+  );
+}
