87d34204d86c7887e4cb354ac4c5025816608ecc
narutiga <100295602+narutiga@users.noreply.github.com>
2022-08-14 19:40:56 +0900

    🐛 import React

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,5 @@
+import React from "react";
 import "src/styles/globals.css";
-import Head from "next/head";
 import { useTodos } from "src/component/State/Todos";
 
 function MyApp({ Component, pageProps }) {
