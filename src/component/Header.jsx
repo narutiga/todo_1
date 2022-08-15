966108385413f12ff04c46ea1818674d295896e8
narutiga <100295602+narutiga@users.noreply.github.com>
2022-08-15 19:06:18 +0900

    🚧 ダミーのhrefを追加

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,16 +4,24 @@ export const Header = () => {
   return (
     <header className="p-4 mb-8 bg-teal-400">
       <Link href="/">
-        <a className="no-underline">
+        <a href="replace" className="no-underline">
           <h1 className="text-white">TODO</h1>
         </a>
       </Link>
       <nav>
         <Link href="/">
-          <a className="no-underline text-white hover:text-gray-500">Home</a>
+          <a
+            href="replace"
+            className="no-underline text-white hover:text-gray-500"
+          >
+            Home
+          </a>
         </Link>
         <Link href="/completed">
-          <a className="no-underline text-white hover:text-gray-500 ml-4">
+          <a
+            href="replace"
+            className="no-underline text-white hover:text-gray-500 ml-4"
+          >
             完了済一覧
           </a>
         </Link>
