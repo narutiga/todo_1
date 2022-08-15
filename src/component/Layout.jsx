3545999c1e10867907268c2e49385e9ac18324cf
narutiga <100295602+narutiga@users.noreply.github.com>
2022-08-15 20:02:31 +0900

    💄 全体をゴシックに

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -3,10 +3,10 @@ import { Footer } from "src/component/Footer";
 
 export const Layout = (props) => {
   return (
-    <div>
+    <div className="font-sans text-base">
       <Header />
 
-      <main className="h-screen w-4/5 mr-auto ml-auto font-sans">
+      <main className="h-screen w-4/5 mr-auto ml-auto">
         {props.input ? props.input : null}
         {props.button ? props.button : null}
         {props.h2}
